6c36af22ac751510dc40b0223dd0faa8d8997fc7
zhamadeh <zeid.hamadeh@hotmail.com>
2021-02-12 13:54:32 -0800

    okay done forreal

--- master.R
+++ master.R
@@ -19,7 +19,7 @@ Aneufinder(inputfolder='../../Sequencing_data/ALL_GOOD_BAM_FILES/', outputfolder
            configfile=NULL, reuse.existing.files=TRUE, binsizes=1e5, stepsizes=1e5, variable.width.reference=NULL, 
            reads.per.bin=NULL, pairedEndReads=TRUE,remove.duplicate.reads=TRUE, min.mapq=10, 
            blacklist="/projects/lansdorp/analyis/aneufinder_blacklist.bed.gz", 
-           use.bamsignals=FALSE, reads.store=FALSE, correction.method="GC", GC.BSgenome = BSgenome.Hsapiens.UCSC.hg19, strandseq=TRUE, 
+           use.bamsignals=FALSE, reads.store=FALSE, strandseq=TRUE, 
            R=10, sig.lvl=0.1, eps=0.01, max.time=60, max.iter=5000, num.trials=15, states=c('zero-inflation',paste0(0:10,'-somy')), 
            confint=NULL, refine.breakpoints=FALSE, hotspot.bandwidth=NULL, hotspot.pval=5e-2, cluster.plots=TRUE)
 
