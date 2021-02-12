a1f26dc9c304e968c56d54d3f999c7e4df47d004
zhamadeh <zeid.hamadeh@hotmail.com>
2021-02-12 12:51:29 -0800

    add blacklist

--- master.R
+++ master.R
@@ -13,12 +13,12 @@ suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(BiocMan
 suppressMessages(suppressPackageStartupMessages(suppressWarnings(BiocManager::install("AneuFinder"))))
 suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(AneuFinder))))
 
-Aneufinder(inputfolder='../../Sequencing_data/ALL_GOOD_BAM_FILES/', outputfolder='aneufinder_output/',
-           numCPU=10, method=c('edivisive', 'dnacopy','HMM'),
-           configfile=NULL, reuse.existing.files=TRUE, binsizes=1e6, stepsizes=1e6, variable.width.reference=NULL, 
+Aneufinder(inputfolder='../../Sequencing_data/ALL_GOOD_BAM_FILES/', outputfolder='aneufinder_output_bl/',
+           numCPU=13, method=c('edivisive', 'dnacopy','HMM'),
+           configfile=NULL, reuse.existing.files=TRUE, binsizes=1e5, stepsizes=1e5, variable.width.reference=NULL, 
            reads.per.bin=NULL, pairedEndReads=TRUE, assembly=NULL, chromosomes=NULL, remove.duplicate.reads=TRUE, min.mapq=10, 
-           blacklist=NULL, 
-           use.bamsignals=FALSE, reads.store=FALSE, correction.method=NULL, GC.BSgenome=NULL, strandseq=TRUE, 
+           blacklist="/projects/lansdorp/analyis/aneufinder_blacklist.bed.gz", 
+           use.bamsignals=FALSE, reads.store=FALSE, correction.method="GC", GC.BSgenome=NULL, strandseq=TRUE, 
            R=10, sig.lvl=0.1, eps=0.01, max.time=60, max.iter=5000, num.trials=15, states=c('zero-inflation',paste0(0:10,'-somy')), 
            confint=NULL, refine.breakpoints=FALSE, hotspot.bandwidth=NULL, hotspot.pval=5e-2, cluster.plots=TRUE)
 
